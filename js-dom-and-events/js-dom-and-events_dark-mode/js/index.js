2263d660e85f257a50284fc6577613889ddab277
Sara Zmak <zmaksara@gmail.com>
2025-03-07 16:10:19 +0100

    select buttons

--- js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
+++ js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,10 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleModeButton = document.querySelector(
+  '[data-js="toggle-mode-button"]'
+);
+
