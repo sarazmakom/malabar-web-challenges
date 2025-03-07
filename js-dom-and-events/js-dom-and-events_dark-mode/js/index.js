6e04f9db6c3d55723cf3a1113fed28df7a1740b7
Sara Zmak <zmaksara@gmail.com>
2025-03-07 16:14:25 +0100

    fix toggle button

--- js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
+++ js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
@@ -4,7 +4,4 @@ const bodyElement = document.querySelector('[data-js="body"]');
 
 const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
 const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
-const toggleModeButton = document.querySelector(
-  '[data-js="toggle-mode-button"]'
-);
-
+const toggleModeButton = document.querySelector('[data-js="toggle-button"]');
