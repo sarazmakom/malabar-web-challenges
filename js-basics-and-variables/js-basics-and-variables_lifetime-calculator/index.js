e4b26e1a6d3b1bc8a6f61162a3d38b404fc01d6c
Sara Zmak <zmaksara@gmail.com>
2025-03-07 12:56:09 +0100

    total number of days lived

--- js-basics-and-variables/js-basics-and-variables_lifetime-calculator/index.js
+++ js-basics-and-variables/js-basics-and-variables_lifetime-calculator/index.js
@@ -20,3 +20,6 @@ Hint: You can combine text and variables using the `+` operator.
 const currentAge = Number(process.argv[2]); // This takes the `<age>` entered in the command `node index.js <age>`.
 
 console.log("Your current age is: " + currentAge);
+
+const daysLived = currentAge * 365;
+console.log(daysLived);
