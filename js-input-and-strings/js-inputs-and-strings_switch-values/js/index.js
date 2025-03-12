5dd9415e0d255bd0176868739c2ff84c0ff90d87
Sara Zmak <zmaksara@gmail.com>
2025-03-12 14:14:41 +0100

    add second input to variable and switch button

--- js-input-and-strings/js-inputs-and-strings_switch-values/js/index.js
+++ js-input-and-strings/js-inputs-and-strings_switch-values/js/index.js
@@ -1,6 +1,7 @@
 console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
 
