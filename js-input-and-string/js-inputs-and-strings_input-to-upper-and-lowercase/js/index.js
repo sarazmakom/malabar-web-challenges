40c9006d1b2cfabd84755b94ee74f095dd5d6425
Sara Zmak <zmaksara@gmail.com>
2025-03-12 10:49:38 +0100

    renamed folder

--- js-input-and-string/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
+++
@@ -1,8 +0,0 @@
-console.clear();
-
-const input = document.querySelector('[data-js="first-input"]');
-const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
-
-uppercaseButton.addEventListener("click", () => {
-  input.value = input.value.toUpperCase();
-});
