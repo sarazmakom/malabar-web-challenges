76898a1b9e8c5ef1cd6692aee60674ab1e5859a0
Sara Zmak <zmaksara@gmail.com>
2025-03-12 12:25:10 +0100

    add event listeners

--- js-input-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-input-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,6 @@
 console.clear();
+
+const colorInput = document.querySelector['data-js="input-color"'];
+const radiusInput = document.querySelector['data-js="input-radius"'];
+const rotationInput = document.querySelector['data-js="input-rotation"'];
+const box = document.querySelector['data-js="box"'];
