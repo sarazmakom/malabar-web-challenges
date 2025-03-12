f4f65dbbe41e8f7fe07c346c92c3160ad3f5cf63
Sara Zmak <zmaksara@gmail.com>
2025-03-12 14:02:48 +0100

    change background color

--- js-input-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-input-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,6 +1,10 @@
 console.clear();
 
-const colorInput = document.querySelector['data-js="input-color"'];
-const radiusInput = document.querySelector['data-js="input-radius"'];
-const rotationInput = document.querySelector['data-js="input-rotation"'];
-const box = document.querySelector['data-js="box"'];
+const colorInput = document.querySelector('[data-js="input-color"]');
+const radiusInput = document.querySelector('[data-js="input-radius"]');
+const rotationInput = document.querySelector('[data-js="input-rotation"]');
+const box = document.querySelector('[data-js="box"]');
+
+colorInput.addEventListener("input", () => {
+  box.style.backgroundColor = `hsl(${colorInput.value}, 70%, 60%)`;
+});
