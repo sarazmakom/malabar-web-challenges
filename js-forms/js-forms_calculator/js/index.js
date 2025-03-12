c1c8a5b55aaedcd0d66a8a2acdc0d2180229e746
Sara Zmak <zmaksara@gmail.com>
2025-03-12 15:44:36 +0100

    js forms calculator

--- js-forms/js-forms_calculator/js/index.js
+++ js-forms/js-forms_calculator/js/index.js
@@ -25,7 +25,28 @@ form.addEventListener("submit", (event) => {
   let result;
 
   // --v-- write your code here --v--
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  const numberA = Number(data.numberA);
+  const numberB = Number(data.numberB);
+  const operator = data.operator;
 
+  if (operator === "addition") {
+    result = add(numberA, numberB);
+  }
+
+  if (operator === "subtraction") {
+    result = subtract(numberA, numberB);
+  }
+
+  if (operator === "multiplication") {
+    result = multiply(numberA, numberB);
+  }
+
+  if (operator === "division") {
+    result = divide(numberA, numberB);
+  }
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
