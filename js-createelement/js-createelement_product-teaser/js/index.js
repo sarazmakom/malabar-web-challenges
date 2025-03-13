a801db15ca1ba45633d4ffa23f19bad637fefad6
Sara Zmak <zmaksara@gmail.com>
2025-03-13 14:45:47 +0100

    add event listener to second button

--- js-createelement/js-createelement_product-teaser/js/index.js
+++ js-createelement/js-createelement_product-teaser/js/index.js
@@ -40,14 +40,7 @@ productTeaser.innerHTML = `<article class="product">
 
 document.body.append(productTeaser);
 
-/*
-  This exercise includes an example of a product teaser for an online shop. 
-  Please append a second product teaser to the page with JavaScript 
-  based on the data given in the `index.js` file.
-
-Use `.innerHTML` to generate the HTML elements for the teaser. 
-Use string interpolation to include the given data into the HTML.
-
-Add an event listener to the "buy" button of the second product teaser.
- When clicking the button, the product's name and price should be logged to the console.
-*/
+const productTeaserButton = productTeaser.querySelector("button");
+productTeaserButton.addEventListener("click", () => {
+  console.log(name, price);
+});
