42857b68f1fc8d36cc93630f0e960b25213fe34a
Sara Zmak <zmaksara@gmail.com>
2025-03-13 13:43:39 +0100

    append new social media post

--- js-createelement/js-createelement_social-media-post/js/index.js
+++ js-createelement/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,39 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const articleElement = document.createElement("article");
+const pElement = document.createElement("p");
+const footerElement = document.createElement("footer");
+const spanElement = document.createElement("span");
+const buttonElement = document.createElement("button");
+
+articleElement.className = "post";
+pElement.className = "post__content";
+footerElement.className = "post__footer";
+spanElement.className = "post__username";
+buttonElement.className = "post__button";
+
+pElement.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+spanElement.textContent = "@username";
+buttonElement.textContent = "♥ Like";
+
+articleElement.append(pElement, footerElement);
+footerElement.append(spanElement, buttonElement);
+
+document.body.append(articleElement);
+
+/* 
+<article class="post">
+      <p class="post__content">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      </p>
+      <footer class="post__footer">
+        <span class="post__username">@username</span>
+        <button type="button" class="post__button" data-js="like-button">
+          ♥ Like
+        </button>
+      </footer>
+    </article>
+*/
