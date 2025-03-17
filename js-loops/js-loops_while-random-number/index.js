e65234241fddac6a954bdc0e09015bb7c5653932
Sara Zmak <zmaksara@gmail.com>
2025-03-17 18:13:27 +0100

    js loops while random number

--- js-loops/js-loops_while-random-number/index.js
+++ js-loops/js-loops_while-random-number/index.js
@@ -4,7 +4,11 @@ let number = 0;
 let count = 0;
 
 // --v-- write or modify code below this line --v--
-
+while (number <= 0.9) {
+  count++;
+  console.log(number);
+  number = Math.random();
+}
 // --^-- write or modify code above this line --^--
 
 console.log(
