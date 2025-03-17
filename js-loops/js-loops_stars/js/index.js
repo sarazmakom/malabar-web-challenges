292fec14e564a41c3e3f2d795addd02856c9d04f
Sara Zmak <zmaksara@gmail.com>
2025-03-17 18:38:46 +0100

    js loops stars part 1

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -7,7 +7,11 @@ function renderStars() {
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
-
+  for (let star = 1; star <= 5; star++) {
+    const emptyStar = document.createElement("img");
+    emptyStar.src = "assets/star-empty.svg";
+    starContainer.appendChild(emptyStar);
+  }
   // --^-- write or modify code above this line --^--
 }
 
