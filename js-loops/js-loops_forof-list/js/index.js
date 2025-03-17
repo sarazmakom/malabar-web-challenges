2747d97f14da2d28c0e4fc91c48d7c6706f4dd6b
Sara Zmak <zmaksara@gmail.com>
2025-03-17 16:17:50 +0100

    js loops for of loop

--- js-loops/js-loops_forof-list/js/index.js
+++ js-loops/js-loops_forof-list/js/index.js
@@ -16,5 +16,9 @@ const programmingLanguages = [
 ];
 
 // --v-- write or modify code below this line --v--
-
+for (const language of programmingLanguages) {
+  const li = document.createElement("li");
+  li.textContent = language;
+  ol.append(li);
+}
 // --^-- write or modify code above this line --^--
