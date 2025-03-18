ffe7d27af8e422d6eb0b5321d137f25dc35e6d60
Sara Zmak <zmaksara@gmail.com>
2025-03-18 15:55:33 +0100

    create components - move Circle to components

---
+++ js-structure/js-structure_shapes/components/Circle/Circle.js
@@ -0,0 +1,10 @@
+export default function Circle() {
+  const circle = document.createElement("div");
+  circle.classList.add("circle");
+  circle.addEventListener("click", () => {
+    circle.style.backgroundColor = getRandomColor();
+  });
+  return circle;
+}
+
+import { getRandomColor } from "../../utils/randomColor.js";
--- js-structure/js-structure_shapes/index.js
+++ js-structure/js-structure_shapes/index.js
@@ -1,17 +1,9 @@
 import { getRandomColor } from "./utils/randomColor.js";
+import Circle from "./components/Circle/Circle.js";
 
 console.clear();
 
 const root = document.getElementById("root");
-
-function Circle() {
-  const circle = document.createElement("div");
-  circle.classList.add("circle");
-  circle.addEventListener("click", () => {
-    circle.style.backgroundColor = getRandomColor();
-  });
-  return circle;
-}
 
 const circleElement = Circle();
 
