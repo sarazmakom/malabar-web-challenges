b9dd951001bc184643b0855f094d2d9acd15802d
Sara Zmak <zmaksara@gmail.com>
2025-03-19 11:57:28 +0100

    remove example data

--- js-fetch/js-fetch_star-wars/js/index.js
+++ js-fetch/js-fetch_star-wars/js/index.js
@@ -3,39 +3,39 @@ import { renderElement } from "./utils.js";
 
 console.clear();
 
-const EXAMPLE_DATA = {
-  name: "Luke Skywalker",
-  height: "172",
-  mass: "77",
-  hairColor: "blond",
-  skin_color: "fair",
-  eye_color: "blue",
-  birth_year: "19BBY",
-  gender: "male",
-  homeworld: "https://swapi.py4e.com/api/planets/1/",
-  films: [
-    "https://swapi.py4e.com/api/films/1/",
-    "https://swapi.py4e.com/api/films/2/",
-    "https://swapi.py4e.com/api/films/3/",
-    "https://swapi.py4e.com/api/films/6/",
-  ],
-  species: [],
-  vehicles: [
-    "https://swapi.py4e.com/api/vehicles/14/",
-    "https://swapi.py4e.com/api/vehicles/30/",
-  ],
-  starships: [
-    "https://swapi.py4e.com/api/starships/12/",
-    "https://swapi.py4e.com/api/starships/22/",
-  ],
-  created: "2014-12-09T13:50:51.644000Z",
-  edited: "2014-12-20T21:17:56.891000Z",
-  url: "https://swapi.py4e.com/api/people/1/",
-};
+// const EXAMPLE_DATA = {
+//   name: "Luke Skywalker",
+//   height: "172",
+//   mass: "77",
+//   hairColor: "blond",
+//   skin_color: "fair",
+//   eye_color: "blue",
+//   birth_year: "19BBY",
+//   gender: "male",
+//   homeworld: "https://swapi.py4e.com/api/planets/1/",
+//   films: [
+//     "https://swapi.py4e.com/api/films/1/",
+//     "https://swapi.py4e.com/api/films/2/",
+//     "https://swapi.py4e.com/api/films/3/",
+//     "https://swapi.py4e.com/api/films/6/",
+//   ],
+//   species: [],
+//   vehicles: [
+//     "https://swapi.py4e.com/api/vehicles/14/",
+//     "https://swapi.py4e.com/api/vehicles/30/",
+//   ],
+//   starships: [
+//     "https://swapi.py4e.com/api/starships/12/",
+//     "https://swapi.py4e.com/api/starships/22/",
+//   ],
+//   created: "2014-12-09T13:50:51.644000Z",
+//   edited: "2014-12-20T21:17:56.891000Z",
+//   url: "https://swapi.py4e.com/api/people/1/",
+// };
 
 // Create dom element for a card and append it to the root
-const firstCard = Card(EXAMPLE_DATA);
-renderElement(firstCard);
+// const firstCard = Card(EXAMPLE_DATA);
+// renderElement(firstCard);
 
 // fetchDataAndRender();
 
@@ -44,8 +44,7 @@ const url = "https://swapi.py4e.com/api/people";
 async function fetchDataAndRender() {
   const response = await fetch(url);
   const data = await response.json();
-  // console.log(data.results);
-  // return data;
+
   data.results.forEach((object) => {
     const card = Card(object);
     renderElement(card);
