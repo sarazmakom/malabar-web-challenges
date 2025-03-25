469d7df8d67ae3cf5a1f418c0563e9241d82d05d
Sara Zmak <zmaksara@gmail.com>
2025-03-25 15:53:25 +0100

    render App, create custom component

--- react-basics/react-basics_hello-world/src/App.jsx
+++ react-basics/react-basics_hello-world/src/App.jsx
@@ -1,5 +1,9 @@
 import "./styles.css";
 
 export default function App() {
-  return <div>Say hello...</div>;
+  return HelloWorld();
+}
+
+export function HelloWorld() {
+  return <h1>Hello World</h1>;
 }
