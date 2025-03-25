3c156293c05b7ef6b0abdf84621b0f9af7222c44
Sara Zmak <zmaksara@gmail.com>
2025-03-25 16:25:30 +0100

    html attributes in react

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,18 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return Article();
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Hello React :)</h2>
+      <label htmlFor="input-field"></label>
+      <input type="text" id="input-field" />
+      <a href="https://react.dev/" className="article__link">
+        Link to React website
+      </a>
+    </article>
+  );
 }
