0aaba70eb8a6b8a908f4b8e39cba17dd763501fc
Sara Zmak <zmaksara@gmail.com>
2025-03-26 11:48:05 +0100

    conditional rendering

--- react-props/react-props_greeting/src/App.jsx
+++ react-props/react-props_greeting/src/App.jsx
@@ -1,11 +1,12 @@
 export default function App() {
-  return <Greeting name="Sunshine" />;
+  return <Greeting name="Esraa" />;
 }
 
 function Greeting({ name }) {
+  const coach = ["Esraa", "Felix", "Gimena", "Klaus", "Marcel", "Sven"];
   return (
     <div>
-      <h1>Hello, {name}!</h1>
+      <h1>Hello, {coach.includes(name) ? "Coach" : name}!</h1>
     </div>
   );
 }
