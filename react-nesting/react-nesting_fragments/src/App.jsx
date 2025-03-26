7a2abb9cdc36a26a891ec8310223f1c615215ce2
Sara Zmak <zmaksara@gmail.com>
2025-03-26 16:13:24 +0100

    replace div with fragment, bring back main element and class to App

--- react-nesting/react-nesting_fragments/src/App.jsx
+++ react-nesting/react-nesting_fragments/src/App.jsx
@@ -3,15 +3,19 @@ import "./styles.css";
 import Box from "./components/Box";
 
 export default function App() {
-  return <Boxes />;
+  return (
+    <main className="flex-container">
+      <Boxes />
+    </main>
+  );
 }
 
 function Boxes() {
   return (
-    <div>
+    <>
       <Box color="#007bff" />
       <Box color="#fc3" />
       <Box color="#ff3333" />
-    </div>
+    </>
   );
 }
