b710fac7286d5a8fa7705353136db5a62f32a46d
Sara Zmak <zmaksara@gmail.com>
2025-03-28 10:42:49 +0100

    react state counter

--- react-state-1/react-state-1_counter/src/App.jsx
+++ react-state-1/react-state-1_counter/src/App.jsx
@@ -1,26 +1,27 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
+  // let count = 0;
+  const [count, setCount] = useState(0);
+
+  function handleCountIncrease() {
+    setCount(count + 1);
+    console.log("increase", count);
+  }
+  function handleCountDecrease() {
+    setCount(count - 1);
+    console.log("decrease", count);
+  }
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleCountDecrease}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleCountIncrease}>
           +
         </button>
       </div>
