35e56bc3a69588a77b91c750f629b17374f10a35
Sara Zmak <zmaksara@gmail.com>
2025-03-31 13:50:14 +0200

    set state on submit, lift state up

--- react-state-2/react-state-2_personal-details/src/components/Form.jsx
+++ react-state-2/react-state-2_personal-details/src/components/Form.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
-export default function Form() {
+export default function Form({ onCreateUser }) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
+    onCreateUser(name, email);
+
+    setName("");
+    setEmail("");
   }
 
   return (
@@ -16,9 +20,23 @@ export default function Form() {
     >
       <h2 id="user-details">Please enter your details here!</h2>
       <label htmlFor="name">Name: </label>
-      <input id="name" name="name" type="text" placeholder="John Doe" />
+      <input
+        id="name"
+        name="name"
+        type="text"
+        placeholder="John Doe"
+        value={name}
+        onChange={(element) => setName(element.target.value)}
+      />
       <label htmlFor="email">Email: </label>
-      <input id="email" name="email" type="email" placeholder="john@doe.com" />
+      <input
+        id="email"
+        name="email"
+        type="email"
+        placeholder="john@doe.com"
+        value={email}
+        onChange={(element) => setEmail(element.target.value)}
+      />
       <button className="form__submit-button" type="submit">
         Submit
       </button>
