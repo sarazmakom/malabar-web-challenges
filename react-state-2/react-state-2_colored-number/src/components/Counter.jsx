d898554dba9aa8c57895866baaa1be6bc183004a
Sara Zmak <zmaksara@gmail.com>
2025-03-31 11:38:54 +0200

    colored numbers

--- react-state-2/react-state-2_colored-number/src/components/Counter.jsx
+++ react-state-2/react-state-2_colored-number/src/components/Counter.jsx
@@ -1,12 +1,21 @@
 import ColoredNumber from "./ColoredNumber";
+import { useState } from "react";
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
+  function handleCountIncrease() {
+    setCount(count + 1);
+  }
+  function handleCountDecrease() {
+    setCount(count - 1);
+  }
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={count} />
       <div className="counter__buttons-container">
         <button
+          onClick={handleCountIncrease}
           type="button"
           className="counter__button"
           aria-label="increment count"
@@ -14,6 +23,7 @@ export default function Counter() {
           +
         </button>
         <button
+          onClick={handleCountDecrease}
           type="button"
           className="counter__button"
           aria-label="decrement count"
@@ -24,3 +34,47 @@ export default function Counter() {
     </div>
   );
 }
+
+/* 
+
+function Parent() {
+  const [count, setCount] = useState(0);
+
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+
+  return <Child count={count} onIncrement={handleIncrement} />;
+}
+function Child({ count, onIncrement }) {
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={onIncrement}>increment</button>
+    </>
+  );
+}
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleCountIncrease() {
+    setCount(count + 1);
+  }
+  function handleCountDecrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <div className="button-container">
+        <button type="button" onClick={handleCountDecrease}>
+          -
+        </button>
+        <button type="button" onClick={handleCountIncrease}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+} */
