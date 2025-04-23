b9a6cbeddd41b54a848ec139ea6596f1e5f20aea
Sara Zmak <zmaksara@gmail.com>
2025-04-23 12:19:41 +0200

    refactor to fetch data from mongoDB

--- backend-read/backend-read_products/pages/api/products/[id].js
+++ backend-read/backend-read_products/pages/api/products/[id].js
@@ -1,14 +1,20 @@
-import { products } from "../../../lib/products";
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
+
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
+  if (request.method === "GET") {
+    const product = await Product.findById(id);
 
-  if (!product) {
-    response.status(404).json({ status: "Not Found" });
+    if (!product) {
+      response.status(404).json({ status: "Not Found" });
+      return;
+    }
+    response.status(200).json(product);
     return;
   }
-
-  response.status(200).json(product);
+  response.status(405).json({ status: "Method not allowed." });
 }
